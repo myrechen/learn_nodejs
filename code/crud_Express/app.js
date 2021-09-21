f4a215de7aa0bea71908ba2dface454186c7e261
myrechen <1972375779@qq.com>
2021-09-21 16:28:54 +0800

    挂载路由

--- code/crud_Express/app.js
+++ code/crud_Express/app.js
@@ -1,8 +1,27 @@
+/**
+ * app.js 入门模块
+ * 职责：
+ *   创建服务
+ *   做一些服务相关配置
+ *     模板引擎
+ *     body-parser 解析表单 post 请求体
+ *     提供静态资源服务
+ *   挂载路由
+ *   监听端口启动服务
+ */
 //	require
 var express = require('express')
-var fs = require('fs')
+var router = require('./router.js')
+
+var bodyParser = require('body-parser')
 
 var app = express()
+
+//post
+app.use(bodyParser.urlencoded({ extended: false}))
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 //	配置使用模板引擎
 app.engine('html', require('express-art-template'))
@@ -11,22 +30,7 @@ app.engine('html', require('express-art-template'))
 app.use('/node_modules/', express.static('./node_modules/'))
 app.use('/public/', express.static('./public/'))
 
-
-app.get('/', function(req, res) {
-	fs.readFile('./db.json', 'utf8', function(err, data) {
-		if (err) {
-			//readFile 的第二个参数是可选的，传入 utf8 把读取到的文件按照 utf8 编码转成字符
-			//除了这样来转换之外，也可以通过 data.toString() 的方式
-			return res.status(500).send('server error!')
-		}
-		// 从文件中读取到的数据一定是字符串, 所以这里转成对象
-		var students = JSON.parse(data).students
-		res.render('index.html', {
-			fruits: ['Apple','Banana','orange','grape'],
-			students: students
-		})
-	})
-})
+app.use(router)
 
 app.listen(3000, function() {
 	console.log('running at 3000...')
